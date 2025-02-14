131a8cc9c7363929a22b2f5be27f69a2c19c9266
Blake <bbpearsall@gmail.com>
2025-02-14 12:19:53 -0500

    Add tests for initial background color, border, and for border removal after upload. Refine comments

--- Plot.Tests/Components/PartialComponents/ImageInput.cs
+++ Plot.Tests/Components/PartialComponents/ImageInput.cs
@@ -19,19 +19,28 @@ public class ImageInputTest : PageTest
         // Expect the image input to be on the page.
         await Expect(imageInput).ToBeVisibleAsync();
 
-        // Verify that the label exists.
+        // Verify that the upload iamge label exists.
         var label = imageInput.Locator("h5.text-decoration-underline.my-auto.text-center");
         await Expect(label).ToBeVisibleAsync();
 
+        // Verify initial background color of gray and border style of dashed.
+        var initialBackgroundColor = await imageInput.EvaluateAsync<string>("element => getComputedStyle(element).backgroundColor");
+        var initialBorderStyle = await imageInput.EvaluateAsync<string>("element => getComputedStyle(element).borderStyle");
+        Assert.Equal("rgb(242, 242, 242)", initialBackgroundColor); // #f2f2f2 in RGB
+        Assert.Equal("dashed", initialBorderStyle);
+
         var fileInput = Page.Locator("#ExampleImageInput-fileInput");
 
-        // Simulate image file upload based.
-        // await fileInput.SetInputFilesAsync(new[] { @"TestImages/myman.jpg" });
+        // Simulate image file upload.
         await fileInput.SetInputFilesAsync(new[] { "../../../Components/PartialComponents/TestImages/myman.jpg" });
 
         // Verify that the background image is set correctly.
         var backgroundImage = await imageInput.EvaluateAsync<string>("element => getComputedStyle(element).backgroundImage");
         Assert.Contains("data:image/jpeg;base64,", backgroundImage);
+
+        // Verify that the border style is removed after image upload.
+        var updatedBorderStyle = await imageInput.EvaluateAsync<string>("element => getComputedStyle(element).borderStyle");
+        Assert.Equal("none", updatedBorderStyle);
     }
 
 }
