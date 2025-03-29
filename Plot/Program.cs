2c3806c68c9d36dedf60fe3ad6c84d40964d5aaf
Michael Polhill <Mapolhill@gmail.com>
2025-03-29 01:08:31 -0400

    Commit for laptop

--- Plot/Program.cs
+++ Plot/Program.cs
@@ -1,7 +1,14 @@
-using System.Net.Http;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Plot.Data.Models.Env;
+using System.Text;
 using Plot.Components;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+EnvironmentSettings envSettings = new();
+builder.Services.AddScoped<EnvironmentSettings>();
 
 builder.WebHost.UseUrls("http://0.0.0.0:8080");
 
@@ -26,6 +33,35 @@ builder.Services.AddScoped(sp =>
 
 builder.Services.AddScoped<AuthHttpClient>();
 
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+{
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = envSettings.issuer,
+            ValidateAudience = true,
+            ValidAudience = envSettings.audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(envSettings.secret_key)),
+            ValidateLifetime = true,
+        };
+    });
+     
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("Employee", policy => policy.RequireClaim("Role", "3", "2", "1"))
+    .AddPolicy("Manager", policy => policy.RequireClaim("Role", "1", "2"))
+    .AddPolicy("Owner", policy => policy.RequireClaim("Role", "1"));
+    
+builder.Services.AddCascadingAuthenticationState();
+//builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+
+
+builder.Services.AddAuthorizationCore();
 builder.Services.AddServerSideBlazor().AddCircuitOptions(options =>
 {
     options.DetailedErrors = true;
@@ -46,9 +82,9 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
 }
-
+// Registers IAuthenticationService
+app.UseAuthorization();
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
