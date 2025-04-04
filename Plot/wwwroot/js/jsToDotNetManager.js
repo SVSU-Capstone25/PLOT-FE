8e0c4d20b98868080c61d8108fe1d820746a9652
Tris <tjcalay@gmail.com>
2025-04-04 17:34:44 -0400

    Changed "copyFloorsetCard.js" into a general JS to DotNet manager helper for future js -> C# calls. Moved floorset configuration modal into a new file

--- Plot/wwwroot/js/jsToDotNetManager.js
+++ Plot/wwwroot/js/jsToDotNetManager.js
@@ -6,23 +6,21 @@ Copies the floorset card data to a new card.
 var dotNet; //Reference to the DotNet object of the Floorset Editor
 
 
-function floorsetDashboardSetDotNet(dotNetObject)
-{
-    if (dotNetObject === null){
+function setDotNet(dotNetObject) {
+    if (dotNetObject === null) {
         console.log("DotNet argument was null!");
     }
-    else{
+    else {
         dotNet = dotNetObject;
         //console.log("Passed obj: "+dotNetObject)
         //console.log("Stored obj: "+dotNet);
     }
-    
+
 }
 
 //Call the DotNet method to add a duplicate floorset card.
-function floorsetDashboardCopyCard(floorsetName)
-{
+function floorsetDashboardCopyCard(floorsetName) {
     //console.log("Floorset Copy Card!");
     //console.log("Stored obj: "+dotNet);
-    dotNet.invokeMethodAsync("CopyFloorset",floorsetName);
+    dotNet.invokeMethodAsync("CopyFloorset", floorsetName);
 }
