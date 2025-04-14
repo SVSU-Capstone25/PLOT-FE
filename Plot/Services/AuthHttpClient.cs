562a35a3475aac8e25e007aef9aed32f76aa5347
Andrew Miller <chumley207@gmail.com>
2025-04-14 18:17:27 -0400

    Cleaned some debug statements. Frontend functionality to enable getting stores added to UsersHttpClient.

--- Plot/Services/AuthHttpClient.cs
+++ Plot/Services/AuthHttpClient.cs
@@ -2,7 +2,6 @@ using System.Net;
 using Plot.Data.Models.Auth.Registration;
 using Plot.Data.Models.Auth.ResetPassword;
 using Plot.Services;
-using System.Text.Json;
 
 public class AuthHttpClient : PlotHttpClient
 {
@@ -25,16 +24,8 @@ public class AuthHttpClient : PlotHttpClient
 
     public async Task<HttpStatusCode> Register(UserRegistration user)
     {
-        // Log the object as JSON for debugging
-        var json = JsonSerializer.Serialize(user, new JsonSerializerOptions { WriteIndented = true });
-        Console.WriteLine("In AuthHttpClient and JSON body is:\n" + json);
-
         JsonContent body = JsonContent.Create(user);
-        Console.WriteLine("body in Register in AuthHttpClient is " + body);
-        /*
-                var json = JsonSerializer.Serialize(body, new JsonSerializerOptions { WriteIndented = true });
-                Console.WriteLine("In Auth Http Client and JSON body is " + json);
-        */
+
         return await SendPostAsync<HttpStatusCode>("/register", body);
     }
 }
--- Plot/Services/UsersHttpClient.cs
+++ Plot/Services/UsersHttpClient.cs
@@ -23,24 +23,9 @@ public class UsersHttpClient : PlotHttpClient
 
     public async Task<HttpStatusCode> UpdateUserPublicInfo(int userId, UpdatePublicInfoUser user)
     {
-        Console.WriteLine("UpdateUserPublicInfo function in UsersHttpClient.razor");
-
-        // Serialize to string so you can see the actual JSON content
-        string jsonString = JsonSerializer.Serialize(user, new JsonSerializerOptions { WriteIndented = true });
-        Console.WriteLine("JSON body in UpdateUserPublicInfo in UsersHttpClient is:");
-        Console.WriteLine("It is:\n" + jsonString);
-
         JsonContent body = JsonContent.Create(user);
-        Console.WriteLine("JSON body in UpdateUserPublicInfo in UsersHttpClient is");
-        Console.WriteLine(body);
 
-        HttpStatusCode response = await SendPatchAsync($"/public-info/{userId}", body);
-        Console.WriteLine($"Response: {response}");
-
-        return response;
-
-
-        //return await SendPatchAsync($"/public-info/{userId}", body);
+        return await SendPatchAsync($"/public-info/{userId}", body);
     }
 
     public async Task<HttpStatusCode> DeleteUserById(int userId)
@@ -65,25 +50,18 @@ public class UsersHttpClient : PlotHttpClient
     // TODO: Change to be a PUT request
     public async Task<HttpStatusCode> UpdateAccessList(UpdateAccessListRequest updateAccessListRequest)
     {
-        Console.WriteLine("Starting UpdateAccessList(UpdateAccessListRequest updateAccessListRequest)");
-        Console.WriteLine("updateAccessListRequest is");
-        Console.WriteLine(updateAccessListRequest);
         JsonContent body = JsonContent.Create(updateAccessListRequest);
 
         return await SendPostAsync<HttpStatusCode>($"/update-access-list", body);
     }
 
-    public async Task<List<Store>?> GetStoreOfUserById(int userId)
+    public async Task<IEnumerable<Store>?> GetStoreOfUserById(int userId)
     {
-        //Console.WriteLine("Attemping to get stores of user");
-        var response = await SendGetAsync<List<Store>?>($"/stores/{userId}");
-        //Console.WriteLine("RESPONSE is:\n" + response + "\n");
-        return response;
+        return await SendGetAsync<IEnumerable<Store>?>($"/stores/{userId}");
     }
 
-    public async Task<List<Store>?> GetStoresNotForUser(int userId)
+    public async Task<IEnumerable<Store>?> GetStoresNotForUser(int userId)
     {
-        //Console.WriteLine("Attemping to get stores not of user");
-        return await SendGetAsync<List<Store>?>($"/stores-not/{userId}");
+        return await SendGetAsync<IEnumerable<Store>?>($"/stores-not/{userId}");
     }
 }
