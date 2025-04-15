7b86610cd41ac73412cec76493c35ef815666ce9
Clayton Cook <claytonleonardcook+github@gmail.com>
2025-04-15 01:20:07 -0400

    fix add fixture model modal

--- Plot/Services/FixturesHttpClient.cs
+++ Plot/Services/FixturesHttpClient.cs
@@ -53,7 +53,7 @@ public class FixturesHttpClient : PlotHttpClient
     {
         JsonContent body = JsonContent.Create(fixtureModel);
 
-        return await SendPostAsync<HttpStatusCode>($"/create-fixture/{storeId}", body);
+        return await SendPostAsync<HttpStatusCode>($"/create-fixture-model/{storeId}", body);
     }
 
     /// <summary>
